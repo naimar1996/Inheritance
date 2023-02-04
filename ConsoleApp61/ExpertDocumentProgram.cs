bb353e04a73dba2d303d84f88f29460cb367c83a
naimar1996 <naimar@code.edu.az>
2023-02-04 13:44:40 +0400

    Inheritance

---
+++ ConsoleApp61/ExpertDocumentProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp61
+{
+    public class ExpertDocumentProgram : ProDocumentProgram
+    {
+        public override void SaveDocument()
+        { 
+        Console.WriteLine("Document Saved in pdf format");
+        }
+    }
+}
---
+++ ConsoleApp61/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp61
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine();
+            switch (word)
+            {
+                case "basic":
+                    DocumentProgram initialVersion = new DocumentProgram();
+                    initialVersion.OpenDocument();
+                    initialVersion.EditDocument();
+                    initialVersion.SaveDocument();
+                    break;
+                case "pro":
+                    ProDocumentProgram midVersion = new ProDocumentProgram();
+                    midVersion.EditDocument();
+                    midVersion.SaveDocument();
+                    break;
+                case "expert":
+                    ExpertDocumentProgram latestVersion = new ExpertDocumentProgram();
+                    latestVersion.SaveDocument();
+                    break;
+                default:
+                    Console.WriteLine("Enter word correctly,please!");
+                    break;
+
+            }
+        }
+    }
+}
